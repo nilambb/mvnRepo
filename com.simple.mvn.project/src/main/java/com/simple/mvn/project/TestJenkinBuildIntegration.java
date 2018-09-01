2a2c40434b6b5c1a6d3e68cbfc09f88b103a3803
nilambb <nilam.bansode5@gmail.com>
2018-09-01 18:09:44 +0530

    Test Jenkins Integration Build

---
+++ com.simple.mvn.project/src/main/java/com/simple/mvn/project/TestJenkinBuildIntegration.java
@@ -0,0 +1,10 @@
+package com.simple.mvn.project;
+
+public class TestJenkinBuildIntegration {
+
+	public static void main(String[] args) {
+		System.out.println("This is to test the Jenkins integration builds......");
+
+	}
+
+}
